38f0304dfdb60b0125510841224ff4f3cea902e0
Anh N <anguyen.se@gmail.com>
2022-11-14 00:21:46 -0500

    getPlace()
    Fixed function to only pick NoVa cities and counties

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # NoVa Restaurant Bot
  Inspired by https://twitter.com/_restaurant_bot, which is on an international scale.
  Source for https://twitter.com/NoVa_Rests_Bot. 
- This Twitter bot posts restaurants in Northern Virginia daily.
+
+ This Twitter bot posts about random restaurants in Northern Virginia daily. These posts contain the name, address, star rating, Google maps link, and four pictures. 
+
+ Note: At the time of creating this bot, the 'node-fetch' package from v3 is an ESM-only module, so it is unable to be imported with require(). If you want to continue using this package while using require() for your other packages, you will want to 'npm install node-fetch@2'.
--- bot.js
+++ bot.js
@@ -8,6 +8,8 @@ var geolib = require('geolib');
 var config = require('./config');
 var { placeDetails } = require('@googlemaps/google-maps-services-js');
 
+const noVa = ['Alexandria', 'Arlington', 'Fairfax', 'Fairfax Station', 'Falls Church', 'West Falls Church', 'Loudoun Valley Estates', 'Manassas', 'Manassas Park', 'Reston', 'Herndon', 'Annandale', 'Woodbridge', 'McLean', 'Vienna', 'Leesburg', 'Chantilly', 'Centreville', 'Tysons Corner', 'Woodburn', 'North Springfield', 'Springfield', 'West Springfield', 'Sterling', 'Potomac Mills', 'Great Falls', 'Ashburn', 'Oakton', 'Baileys Crossroads', 'Burke', 'Bull Run'];
+
 const T = new Twit({
     consumer_key:         config.TWITTER_consumer_key,
     consumer_secret:      config.TWITTER_consumer_secret,
@@ -15,7 +17,7 @@ const T = new Twit({
     access_token_secret:  config.TWITTER_access_token_secret,
 })
 
-const G = config.GOOGLE_api_key;
+const G_api = config.GOOGLE_api_key;
 
 function tweetIt() {
     var r = Math.floor(Math.random()*100);
@@ -41,5 +43,26 @@ function rand(array) {
     return array[i];
 }
 
-//tweetIt();
-//setInterval(tweetIt, 1000*30);
+function getPlace() {
+	return new Promise(async (resolve, reject) => {
+		const places = cities.filter((city) => (city.adminCode.match('VA') && noVa.includes(city.name)));
+        const countries = places.map((place) => place.country);
+        const country = rand(countries);
+        const countryPlaces = places.filter((place) => place.country === country);
+        const place = rand(countryPlaces);
+		resolve(place.loc.coordinates);
+	});
+}
+
+async function searchNearby(placeCoordinates) {
+	const nearPlaces = await fetch(
+		`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${placeCoordinates[1]},${placeCoordinates[0]}&radius=5000&type=restaurant&key=${G_api}`
+	);
+	const nearArr = await nearPlaces.json();
+
+	const nearFiltered = nearArr.results.filter(
+		(place) => !place.name.includes('Hotel') && !place.types.includes('lodging') && !place.types.includes('gas_station')
+	);
+	const nearOne = rand(nearFiltered);
+	return { placeCoordinates, ...nearOne };
+}
